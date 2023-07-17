ec5158b4afad4324ce8034cefe16d0a512149ee7
Duy Hung Nguyen <21020436@vnu.edu.vn>
2023-07-17 20:52:11 +0700

    Delete measure freq test module

--- freq_measure_t/GNU ARM v10.3.1 - Debug/gecko_sdk_4.1.0/platform/service/iostream/src/sl_iostream_stdlib_config.d
+++
@@ -1,4 +0,0 @@
-gecko_sdk_4.1.0/platform/service/iostream/src/sl_iostream_stdlib_config.o: \
- C:/Users/bannh/SimplicityStudio/SDKs/gecko_sdk/platform/service/iostream/src/sl_iostream_stdlib_config.c \
- c:\users\bannh\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h
-c:\users\bannh\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
--- freq_measure_t/GNU ARM v10.3.1 - Debug/gecko_sdk_4.1.0/platform/service/system/src/sl_system_init.d
+++
@@ -1,6 +0,0 @@
-gecko_sdk_4.1.0/platform/service/system/src/sl_system_init.o: \
- C:/Users/bannh/SimplicityStudio/SDKs/gecko_sdk/platform/service/system/src/sl_system_init.c \
- c:\users\bannh\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
- E:\BGM220\ Projects\freq_measure_t\autogen/sl_event_handler.h
-c:\users\bannh\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
-E:\BGM220\ Projects\freq_measure_t\autogen/sl_event_handler.h:
